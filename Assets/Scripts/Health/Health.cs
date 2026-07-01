becc0234ea705506db741c16f4c97e825b1a5805
Usama khan <ukusama473@gmail.com>
2026-07-01 14:02:34 +0500

    Added game graphics
    Added new sprites and visual assets for the game

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+   [SerializeField] private float startingHealth;
+   private float currentHealth;
+  private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        if (currentHealth > 0)
+        {
+            // Player hurt
+        }
+        else
+        {
+            // Player dead
+        }
+    }
+
+
+}
